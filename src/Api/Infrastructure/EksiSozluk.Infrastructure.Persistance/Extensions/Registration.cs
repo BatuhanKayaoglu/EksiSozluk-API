2fdc66bc16cc92a33ed39a292c4b653f883378c7
Batuhan Kayaoğlu <112872391+BatuhanKayaoglu@users.noreply.github.com>
2024-02-24 15:05:41 +0300

    Add project files.

---
+++ src/Api/Infrastructure/EksiSozluk.Infrastructure.Persistance/Extensions/Registration.cs
@@ -0,0 +1,33 @@
+﻿using EksiSozluk.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksiSozluk.Infrastructure.Persistance.Extensions
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EksiSozlukContext>(conf =>
+            {
+                var connStr = configuration["EksiSozlukDbConnectionString"].ToString();
+                conf.UseSqlServer(connStr);
+            });
+
+
+            // bu kısmı sonradan ekledik seedData olusturup db'ye eklemek için.
+            var seedData = new SeedData();
+            seedData.SeedAsync(configuration).GetAwaiter().GetResult();
+
+            return services;
+
+        }
+    }
+}
