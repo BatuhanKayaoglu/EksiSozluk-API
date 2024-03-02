64d7b7f6b2c636281b0a58c9b977a870785bbfe6
Batuhan Kayaoğlu <112872391+BatuhanKayaoglu@users.noreply.github.com>
2024-03-03 00:53:55 +0300

    ExceptionHanding add

--- src/Api/WebApi/EksiSozluk.Api.WebApi/Program.cs
+++ src/Api/WebApi/EksiSozluk.Api.WebApi/Program.cs
@@ -1,6 +1,7 @@
 using EksiSozluk.Infrastructure.Persistance.Extensions;
 using EksiSozluk.Api.Application.Extensions;
 using FluentValidation.AspNetCore;
+using EksiSozluk.Api.WebApi.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructureRegistration(builder.Configuration);
 builder.Services.AddApplicationRegistration();
+builder.Services.ConfigureAuth(builder.Configuration);
 
 
 var app = builder.Build();
@@ -29,6 +31,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.ConfigureExceptionHandling(app.Environment.IsDevelopment());   
+
+app.UseAuthentication();    
 app.UseAuthorization();
 
 app.MapControllers();
